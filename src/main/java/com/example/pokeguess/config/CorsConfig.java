8e06d6eff0b14fde2c5fa7f90468b3890717888b
zhengxuan zhao <zzhao260985652@gmail.com>
2026-01-17 14:20:42 -0500

    update vercel url

--- src/main/java/com/example/pokeguess/config/CorsConfig.java
+++ src/main/java/com/example/pokeguess/config/CorsConfig.java
@@ -15,8 +15,7 @@ public class CorsConfig {
                 registry.addMapping("/api/**")
                         .allowedOrigins(
                                 "http://localhost:3000",
-                                "https://pokeguess-teal.vercel.app",
-                                "https://https://pokeguess-ehn9.vercel.app/"  // Add your Vercel URL here
+                                "https://pokeguess-ehn9.vercel.app"  // Add your Vercel URL here
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
